369c6be3d831ac339061642a62a621d2b64c4bff
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2017-02-08 17:32:45 -0500

    fix quick submodule loader for geojson

--- index.js
+++ index.js
@@ -3,15 +3,7 @@
 let submodules = {
 	'less-live-load': require ('./src/submodules/less-live-load'),
 	'form-data-body-parser': require ('./src/submodules/multipart-body-parser'),
-  'geo-json': {
-    Geometry: require ('./src/submodules/Geometry'),
-    Coordinate: require ('./src/submodules/Coordinate'),
-    Position: require ('./src/submodules/Position'),
-    Point: require ('./src/submodules/Point'),
-    MultiPoint: require ('./src/submodules/MultiPoint'),
-    LineString: require ('./src/submodules/LineString'),
-    MultiLineString: require ('./src/submodules/MultiLineString')
-  },
+  'geo-json': require ('./src/submodules/GeoJSON'),
   'join-js': require ('./src/submodules/join-js'),
   'client-library': require ('express').static ('src/client side')
 }
