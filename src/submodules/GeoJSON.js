a9e8af3ba4fc3e81c622618316fa9351c788ad84
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2017-02-12 00:28:00 -0500

    geojson parse

--- src/submodules/GeoJSON.js
+++ src/submodules/GeoJSON.js
@@ -3,4 +3,5 @@ exports.MultiPoint = require ('./MultiPoint');
 exports.LineString = require ('./LineString');
 exports.MultiLineString = require ('./MultiLineString');
 exports.Polygon = require ('./Polygon');
+exports.Parser = require ('./Parser');
 exports.geo = require ('../client side/geolocation');
